051f653ce1b786ac5fe6895ceecf2c2477c9c88a
Papangping <papangping@gmail.com>
2014-07-28 06:52:41 +0700

    create index.php create style.css
    create main layout

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Papangping
+ * Date: 7/28/14
+ * Time: 5:46 AM
+ */
+?>
+<!DOCTYPE>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="pnp-gray-bg web-top-section"></div>
+    <div class="wrap">
+
+    </div>
+</body>
+</html>
