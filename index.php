e81a433b0decdfe1469fa2f35509f8b8d36e8ca9
Papangping <papangping@gmail.com>
2014-07-28 08:14:29 +0700

    complete header layout add navigator tag

--- index.php
+++ index.php
@@ -11,11 +11,34 @@
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/style.css">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
 </head>
 <body>
     <div class="pnp-gray-bg web-top-section"></div>
-    <div class="wrap">
+    <div class="fixed-header">
+        <div id="bg_header" class="wrapper">
+            <a href=""><div id="logo" class="pull-left"></div></a>
+            <div id="social_top" class="pull-right"></div>
+            <div class="clearfix"></div>
+            <div id="navigation">
+                <a href="index.php?page=home"></a>
+                <a href="index.php?page=who_we_are"></a>
+                <a href="index.php?page=product_service"></a>
+                <a href="index.php?page=size_chart"></a>
+                <a href="index.php?page=how_to_order"></a>
+                <a href="index.php?page=fabric"></a>
+                <a href="index.php?page=contact"></a>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper" style="height: 1500px;">
 
     </div>
 </body>
 </html>
+<style>
+</style>
