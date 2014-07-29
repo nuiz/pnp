0f779389306f03c36f79cb2674a869224e7864ae
aommiez <aommiez@gmail.com>
2014-07-29 22:57:35 +0700

    add admin adasd adas

---
+++ admin/slide.php
@@ -0,0 +1,9 @@
+<div class="page-header">
+    <h1>Add Images</h1>
+</div>
+<p>
+    <form action="slide_upload.php" method="post" enctype="multipart/form-data" name="form1" id="form1">
+        <input name="ufile" type="file" id="ufile" style="display: inline-block;width: 76px;"> เลือกไฟล์รูปสำรูป Slide Show หน้าแรก แล้วกดปุ่ม Upload Image
+        <input type="submit" value="Upload Image">
+    </form>
+</p>
